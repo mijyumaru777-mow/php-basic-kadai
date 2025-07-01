e7bd86fde0ab625e6108da12d7b10536e190b464
mijyumaru777-mow <misuzu.gyo_m7@icloud.com>
2025-07-01 12:36:07 +0900

    kadai_016を追加

---
+++ kadai_016/class.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎</title>
+</head>
+<body>
+    <p>
+        <?php
+        class Food{
+            private $name;
+            private $price;
+
+            //コンストラクタを追加
+            function_construct(string $name, $price){
+                $this->name = $name;
+                $this->price = $price;
+            }
+            //show_priceメソッド
+            function show_price(){
+                echo $this->price.'<br>';
+            }
+        }
+
+        //Foodクラスのインスタンスを作成
+        $potato = new Food("potato",250);
+        $potato->show_price();
+        print_r($potato);
+
+        class Animal{
+            private $name;
+            private $height;
+            private $weight;
+
+            //コンストラクタを追加
+            function_construct(string $name, $height, $weight){
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+            //show_heightメソッド
+            function show_height(){
+                echo $this->height.'<br>';
+            }
+        }
+
+        //Animalクラスのインスタンスを作成
+        $lion = new Animal("lion",110,190);
+        $lion->show_height();
+        print_r($lion);
+        ?>
+</body>
+</html>
